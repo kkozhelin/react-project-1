f3412cc9a3317fa765e759cf82bb3b7325351df0
Kozhelin Kirill <k.kozhelin@gmail.com>
2026-03-01 16:30:23 +0300

    добавил кнопку, тайтал и дискриптион

--- eslint.config.js
+++ eslint.config.js
@@ -4,26 +4,47 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default [
+  { ignores: ['dist'] },
   {
     files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser
+      },
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
+        sourceType: 'module'
+      }
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'react': react
     },
     rules: {
+      ...js.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      ...react.configs.recommended.rules,
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-    },
-  },
-])
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true }
+      ],
+      'semi': ['error', 'always', { 'omitLastInOneLineBlock': false}],
+      'comma-dangle': ['error', 'never'],
+      'quotes': ['error', 'single'],
+      'react/prop-types': [0],
+      'indent': ['error', 'tab'],
+      'react/jsx-no-target-blank': 'error',
+      'react/react-in-jsx-scope': 'off'
+    }
+  }
+];
