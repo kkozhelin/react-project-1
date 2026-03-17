5a9027fdc8a0cc691f82357a33d6b83e7f055204
Kozhelin Kirill <k.kozhelin@gmail.com>
2026-03-17 19:49:23 +0300

    добавил header

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import './App.css'
 import Button from './components/Button/Button.jsx';
 import Title from './components/Title/Title.jsx';
 import Description from "./components/Description/Description.jsx";
-import Input from "./components/input/Input.jsx";
+import Input from "./components/IInput/Input.jsx";
+import Header from "./components/Header/Header.jsx";
 
 function App() {
     const saveChange = () => {
@@ -12,6 +13,7 @@ function App() {
 
   return (
     <>
+        <Header/>
         <Title>Поиск</Title>
         <Button onClick={ saveChange }>Искать</Button>
         <Description>Введите название фильма, сериала или мультфильма для поиска и добавления в избранное.</Description>
